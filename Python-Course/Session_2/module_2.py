204bcead91a200a6ec638592bb656e5e97bdcd69
Gulem Toktamys <glimtoktamys82@gmail.com>
2026-01-13 14:11:40 +0500

    Completed Session 2 hometasks

--- Python-Course/Session_2/module_2.py
+++ Python-Course/Session_2/module_2.py
@@ -4,32 +4,87 @@ from typing import Any, Dict, List, Tuple
 
 
 def task_1(data_1: Dict[str, int], data_2: Dict[str, int]):
-    pass
+    result = data_1.copy()
+
+    for key, value in data_2.items():
+        if key in result:
+            result[key] += value
+        else:
+            result[key] = value
+
+    return result
 
 
 def task_2():
-    pass
+    result = {}
+    for i in range(1, 16):
+        result[i] = i * i
+    return result
 
 
 def task_3(data: Dict[Any, List[str]]):
-    pass
+    result = [""]
+
+    for values in data.values():
+        new_result = []
+        for prefix in result:
+            for letter in values:
+                new_result.append(prefix + letter)
+        result = new_result
+    return result
+
 
 
 def task_4(data: Dict[str, int]):
-    pass
+    sorted_items = sorted(data.items(), key=lambda item: item[1], reverse=True)
+
+    result = []
+    for key, value in sorted_items[:3]:
+        result.append(key)
+
+    return result
 
 
 def task_5(data: List[Tuple[Any, Any]]) -> Dict[str, List[int]]:
-    pass
+    result = {}
+    for key, value in data:
+        if key not in result:
+            result[key] = []
+        result[key].append(value)
+
+    return  result
 
 
 def task_6(data: List[Any]):
-    pass
+    result = []
+
+    for item in data:
+        if item not in result:
+            result.append(item)
+
+    return result
 
 
 def task_7(words: [List[str]]) -> str:
-    pass
+    if not words:
+        return ""
+
+    prefix = words[0]
+
+    for word in words[1:]:
+        while not word.startswith(prefix):
+            prefix = prefix[:-1]
+            if prefix == "":
+                return ""
+    return prefix
 
 
 def task_8(haystack: str, needle: str) -> int:
-    pass
+    if needle == "":
+        return 0
+
+    n, m = len(haystack), len(needle)
+    for i in range(n - m + 1):
+        if haystack[i: i + m] == needle:
+            return i
+    return -1
