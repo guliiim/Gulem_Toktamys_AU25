905b9be63177c10857a49f3c302d13f35c49e284
Gulem Toktamys <glimtoktamys82@gmail.com>
2026-01-14 13:08:52 +0500

    Completed Session 3 hometasks

--- Python-Course/Session_3/module_3.py
+++ Python-Course/Session_3/module_3.py
@@ -1,19 +1,29 @@
-# import time
+import time
 from typing import List
 
 Matrix = List[List[int]]
 
 
 def task_1(exp: int):
-    pass
+    def power_factory(num: int) -> int:
+        return num ** exp
+    return power_factory
 
 
 def task_2(*args, **kwags):
-    pass
+    for value in args:
+        print(value)
+    for value in kwags.values():
+        print(value)
 
 
 def helper(func):
-    pass
+    def wrapper(name):
+        print("Hi, friend! What's your name?")
+        result = func(name)
+        print("See you soon!")
+        return result
+    return wrapper
 
 
 @helper
@@ -22,8 +32,14 @@ def task_3(name: str):
 
 
 def timer(func):
-    pass
-
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+        return result
+    return wrapper
 
 @timer
 def task_4():
@@ -31,8 +47,27 @@ def task_4():
 
 
 def task_5(matrix: Matrix) -> Matrix:
-    pass
+    rows = len(matrix)
+    columns = len(matrix[0])
 
+    result = []
+    for j in range(columns):
+        new_row = []
+        for i in range(rows):
+            new_row.append(matrix[i][j])
+        result.append(new_row)
+
+    return result
 
 def task_6(queue: str):
-    pass
+    balance = 0
+    for item in queue:
+        if item == '(':
+            balance += 1
+        elif item == ')':
+            balance -= 1
+
+        if balance < 0:
+            return False
+    return balance == 0
+
